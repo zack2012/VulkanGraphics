b55fa70a455691eb5c44aa1b3de9688e93176f15
zack <zbhne@icloud.com>
2018-10-11 09:12:00 +0800

    init commit

---
+++ cmake-build-debug/CMakeFiles/VulkanGraphics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanGraphics.dir/main.cpp.o"
+  "VulkanGraphics.pdb"
+  "VulkanGraphics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanGraphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
